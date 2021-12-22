434d9405e3ffb3c549deaa8757535f528b067faa
optimalchief <82474556+optimalchief@users.noreply.github.com>
2021-12-22 13:08:20 -0500

    Add apiUrl
    Added apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9f9b53db.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
